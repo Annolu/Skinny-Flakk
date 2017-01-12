34f184b8b118c72d66b588f0d6b275e7ec2563d6
Noemi <andreoli.noemi@gmail.com>
2017-01-12 18:48:49 +0000

    finished the tour section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log("Skinny Flakk");
+var buttonViewMore= document.getElementById("dates-button");
+var tourWrapper= document.getElementById("tour-wrapper");
+var listOfDates= document.getElementById("list-of-dates");
+
+buttonViewMore.addEventListener("click", openConcerts)
+
+function openConcerts(){
+    
+    if(tourWrapper.classList.contains("open-wrapper")){
+        tourWrapper.classList.remove("open-wrapper");
+        listOfDates.classList.remove("open-list");
+    }else{
+        tourWrapper.classList.add("open-wrapper");
+        listOfDates.classList.add("open-list");
+        buttonViewMore.innerHTML= "VER MENOS FECHAS";
+    }
+
+    
+    
+}
+
