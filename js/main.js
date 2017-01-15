f20c654d652d48c9d01743428cd1a38d94e249a0
Noemi <andreoli.noemi@gmail.com>
2017-01-15 10:24:07 +0000

    added arrows to the carousel and changing colour top-menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var buttonViewMore= document.getElementById("dates-button");
 var tourWrapper= document.getElementById("tour-wrapper");
 var listOfDates= document.getElementById("list-of-dates");
-var topMenu=document.getElementById("topMenu");
-var topMenuNav=document.getElementById("topMenuNav");
-var ulLi=document.getElementById("ulLi");
+var topMenuTabs=document.getElementById("topMenuTabs");
+var headerSectionHeight= document.getElementById("headerSection").offsetHeight;
+var tourSectionHeight= document.getElementById("tourSection").offsetHeight;
+var cdSectionHeight= document.getElementById("cdsSection").offsetHeight;
+var topMenuHeight= document.getElementById("topMenu").offsetHeight;
+
+
+
+// open the "see more dates" tab in the tour section
 
 buttonViewMore.addEventListener("click", openConcerts)
 
@@ -12,6 +18,8 @@ function openConcerts(){
     if(tourWrapper.classList.contains("open-wrapper")){
         tourWrapper.classList.remove("open-wrapper");
         listOfDates.classList.remove("open-list");
+        buttonViewMore.innerHTML= "VER TODAS LAS FECHAS";
+        
     }else{
         tourWrapper.classList.add("open-wrapper");
         listOfDates.classList.add("open-list");
@@ -19,24 +27,47 @@ function openConcerts(){
     }
 }
 
-$('.owl-carousel').owlCarousel({
-    center: true,
-    items:2,
-    loop:true,
-    autoWidth:true,
-    responsive:{
-        600:{
-            items:2
-        }
-    }
-});
 
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
+
+$(document).ready(function() {
+// owl carousel    
+    
+    var owl = $("#owl-demo");
+
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        items:2,
+        loop:true,
+        dots:false,
+        autoWidth:true,
+        responsive:{
+            600:{
+                items:2
+            }
+        }
+    });  
+    
+//owl carousel gets linked to the arrows
+    
+    owl.owlCarousel();
+    // Go to the next item
+    $('.right-arrow').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.left-arrow').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+    
+//smooth scroll to internal anchor link    
+    
+    $('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
 	    var target = this.hash;
-	    var $target = $(target);
+	    var $target = $(target);        
 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
@@ -44,37 +75,64 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
-});
-
-
-/*
-document.addEventListener("scroll", appearToMenu)
-
-function appearToMenu(){
-    topMenu.classList.add("top-menu-down")
-}*/
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-         $('.top-menu').addClass('top-menu-down');
-    } else {
-         $('.top-menu').removeClass('top-menu-down');
-    }
     
-    if ($(this).scrollTop() > 600){
-        go(ulLi.children[1].children[0]);
-        console.log($(this).scrollTop())
-    }else{
-        if ($(this).scrollTop() > 1400){
-            go(ulLi.children[2].children[0]); 
+// video carousel     
+    $('.owl-carousel2').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:10,
+        video:true,
+        lazyLoad:true,
+        center:true,
+        responsive:{
+            480:{
+                items:2
+            },
+            600:{
+                items:4
+            }
         }
-    }    
+    })
+
 });
 
-function go(dot){
+//positions at which the top-menu tabs change colour and show/hide fixed top-menu
+
+$(window).scroll(function() {  
+    
+    var distanceFromTop= $(this).scrollTop();    
+    
+    var offsetHeight= headerSection.offsetHeight;    
+    
+    switch(true){
+        case (distanceFromTop<topMenuHeight):
+            $('.top-menu').removeClass('top-menu-down');            
+            topMenuTabs.children[0].children[0].classList.remove("is-active");
+            break;          
+
+        case (distanceFromTop<headerSectionHeight && distanceFromTop>topMenuHeight):
+            $('.top-menu').addClass('top-menu-down');            
+            go(topMenuTabs.children[0].children[0]);
+            break;
+            
+        case (distanceFromTop<headerSectionHeight+tourSectionHeight &&                        distanceFromTop>headerSectionHeight):
+            go(topMenuTabs.children[1].children[0]);
+            break;
+            
+        case (distanceFromTop>tourSectionHeight+headerSectionHeight):
+            go(topMenuTabs.children[2].children[0]);
+            break;
+    }
+});
+
+//function that changes the top-menu tabs' colours
+
+function go(tabToActivate){
     setTimeout(function(){
-        dot.classList.remove("is-active");
-        console.log(dot)
-        dot.parentElement.nextElementSibling.children[0].classList.add("is-active");
-    }, 600);
+            for (var item of topMenuTabs.children){
+                item.children[0].classList.remove("is-active");
+            }
+        tabToActivate.classList.add("is-active");
+    }, 200);
 }
